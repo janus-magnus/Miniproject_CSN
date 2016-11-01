b7ef1886979a99a2b30e9751ebec1bae570938f1
janus-magnus <Ykorringa@gmail.com>
2016-11-01 10:36:53 +0100

    new alarm

--- Main/Alarm.py
+++ Main/Alarm.py
@@ -1,23 +1,33 @@
 import RPi.GPIO as GPIO
 import time
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BOARD)#pin setups
 GPIO.setup(11, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 GPIO.setup(12, GPIO.OUT)
-GPIO.output(12, 0)
+GPIO.setup(37, GPIO.OUT)
 
-lightOnTime = 2#hardcoded for now, should be changeable by the user
-lightOffTime = 2##hardcoded for now, should be changeable by the user
 
-try:
+GPIO.setup(36, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.output(37, 1)#set pin 37 aan zodat hij als 3.3V output werkt
+
+#aanpasbaare blink time/ nu nog hardcoded
+LED_off_time = 2
+LED_on_time = 2
+
+
+def Aon():
     while True:
-        if(GPIO.input(11)==1):#buttonpress
-            while True:
-                GPIO.output(12, 1)
-                time.sleep(lightOnTime)
-                GPIO(12, 0)
-                time.sleep(lightOffTime)
+            if GPIO.input(36):#button2 moet ingedrukt blijven worden tot hij stopt
+                break
+            GPIO.output(12, 1)
+            time.sleep(LED_on_time)
+            GPIO.output(12, 0)
+            time.sleep(LED_off_time)
 
 
-except KeyboardInterrupt:
-    GPIO.cleanup()
+while True:
+    if GPIO.input(11) == 1:
+        Aon()
+
+GPIO.cleanup()
